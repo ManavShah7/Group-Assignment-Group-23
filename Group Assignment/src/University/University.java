13d3dbb57a046957af5c60783956dcebc6eb05c5
ManavShah7 <shah.manavd@northeastern.edu>
2025-10-25 16:25:31 -0400

    Created Packages

---
+++ Group Assignment/src/University/University.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package University;
+
+import University.CourseCatalog.Course;
+import University.CourseCatalog.CourseCatalog;
+import info5100.university.example.CourseSchedule.CourseLoad;
+import info5100.university.example.CourseSchedule.CourseOffer;
+import info5100.university.example.CourseSchedule.CourseSchedule;
+import University.Department.Department;
+import University.Persona.Person;
+import University.Persona.PersonDirectory;
+import University.Persona.StudentDirectory;
+import University.Persona.StudentProfile;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class University {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Department department = new Department("Information Systems");
+        CourseCatalog coursecatalog = department.getCourseCatalog();
+        
+        Course course = coursecatalog.newCourse("app eng", "info 5100", 4);
+        
+        CourseSchedule courseschedule = department.newCourseSchedule("Fall2020");
+
+        CourseOffer courseoffer = courseschedule.newCourseOffer("info 5100");
+        if (courseoffer==null)return;
+        courseoffer.generatSeats(10);
+        PersonDirectory pd = department.getPersonDirectory();
+        Person person = pd.newPerson("0112303");
+        StudentDirectory sd = department.getStudentDirectory();
+        StudentProfile student = sd.newStudentProfile(person);
+        CourseLoad courseload = student.newCourseLoad("Fall2020"); 
+//        
+        courseload.newSeatAssignment(courseoffer); //register student in class
+        
+        int total = department.calculateRevenuesBySemester("Fall2020");
+        System.out.print("Total: " + total);
+
+    }
+
+}
